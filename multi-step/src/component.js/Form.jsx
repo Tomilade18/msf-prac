9aaba574f4e8bc37fbe5f449be403d597bd5c0c3
Tomilade18 <jboyyumzey@gmail.com>
2025-08-09 13:26:30 +0100

    first commit

---
+++ multi-step/src/component.js/Form.jsx
@@ -0,0 +1,16 @@
+export default function Form(){
+
+    return(
+        <div>
+            <div className="progressbar"></div>
+            <div className="form-container">
+               <div className="header"></div>
+               <div className="body"></div>
+               <div className="footer">
+                <button>prev</button>
+                <button>next</button>
+               </div>
+            </div>
+        </div>
+    )
+}
